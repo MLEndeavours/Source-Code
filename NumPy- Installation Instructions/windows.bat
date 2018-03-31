694cec6fe86a9431742d44b8f2c6dc8629babcc1
Suyog Jadhav <suyog.jadhav1@gmail.com>
2018-03-31 19:36:03 +0530

    Create windows.bat

---
+++ NumPy- Installation Instructions/windows.bat
@@ -0,0 +1,11 @@
+:: The native way
+pip install numpy
+
+:: If above fails, download get-pip.py
+:: from https://bootstrap.pypa.io/get-pip.py
+:: Now, change to the the directory where you just downloaded it and do this-
+python get-pip.py
+
+:: Using Anaconda. Search for "Anaconda Prompt" and
+:: run it. Then type in-
+conda update numpy
