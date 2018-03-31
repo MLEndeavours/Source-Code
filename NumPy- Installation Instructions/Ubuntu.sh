7705ae78d84ee626446cbd5d49489a85ab5c18bd
Suyog Jadhav <suyog.jadhav1@gmail.com>
2018-03-31 19:29:18 +0530

    Create Ubuntu.sh

---
+++ NumPy- Installation Instructions/Ubuntu.sh
@@ -0,0 +1,10 @@
+# the native way
+sudo pip install numpy
+
+# if above fails-
+sudo apt-get install python-pip
+# or for Python3-
+sudo apt-get install python3-pip
+
+# using Anaconda
+conda update numpy
